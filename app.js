53165a7ce860abebdb56244b9d895c71c8a65ebb
Matheus Zali <66894179+ZXL7@users.noreply.github.com>
2022-03-14 22:35:01 -0300

    Funcao de alerta

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
-    produto: false
+    produto: false,
+    compras: [],
+    mensagem: '',
+    alertaAtivo: false,
   },
   filters: {
     numbersforPrice(valor) {
       return valor.toLocaleString("pt-BR",{style:"currency", currency:"BRL"} )
     },
+  },
+  computed: {
+    carrinho() {
+      let contador = 0
+      if(this.compras.length) {
+        this.compras.forEach(item => {
+          contador += item.preco
+        })
+      }
+      return contador
+    }
   },
   methods: {
     renderProducts() {
@@ -24,12 +38,48 @@ const vm = new Vue({
         this.produto = r
       })
     },
+    openModal(id) {
+      this.renderproduct(id)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
     closeModal({ target, currentTarget }) {
       if(currentTarget === target) 
       this.produto = false
+    },
+    pushProduct() {
+    this.produto.estoque--
+    const {id,preco,nome} = this.produto
+    this.compras.push({id,nome,preco})
+    this.openMessage(`${nome} foi somado ao carrinho`)
+    },
+    removeritem(index){
+      this.compras.splice(index, 1)
+      console.log({index})
+    },
+    checkarValorLocalStorage(){
+      if(window.localStorage.compras) {
+        this.compras = JSON.parse(window.localStorage.compras)
+      }
+    },
+    openMessage(mensagem) {
+      this.mensagem = mensagem
+      this.alertaAtivo = true
+      setTimeout(() => {
+        this.alertaAtivo = false
+      }, 1500)
     }
-  },
+},
   created() {
     this.renderProducts();
+    this.checkarValorLocalStorage();
   },
+  watch: {
+    carrinho () {
+      window.localStorage.compras = JSON.stringify(this.compras)
+      console.log('Adicionou algo ao localStorage')
+    }
+  }
 })
